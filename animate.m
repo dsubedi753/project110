e9d9e063de88b1501772280b682fb872da7b9c22
thunder753 <thunder753@users.noreply.github.com>
2019-04-27 20:08:41 -0400

    'Forced_3d_view'

--- animate.m
+++ animate.m
@@ -5,6 +5,7 @@ curve3 = animatedline('Color','g');
 
 set(gca, 'XLim', [-20,20],'YLim',[-20,20],'ZLim',[-20,20]);
 grid on
+view(3);
 
 for i = 1:length(X1)
     addpoints(curve1,X1(i), Y1(i), Z1(i));

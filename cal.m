96b0ecbe86787c5a18b37c7ca3575a368736d443
thunder753 <thunder753@users.noreply.github.com>
2019-04-27 20:18:43 -0400

    'correction_of_signs'

--- cal.m
+++ cal.m
@@ -11,11 +11,11 @@ R12 = (D12(1)^2 + D12(2)^2 + D12(3)^2)^(1/2);    %Distance between bodies 1 and 
 R13 = (D13(1)^2 + D13(2)^2 + D13(3)^2)^(1/2);    %Distance between bodies 1 and 3
 R23 = (D23(1)^2 + D23(2)^2 + D23(3)^2)^(1/2);    %Distance between bodies 2 and 3
 
-FG12 = - G*M1*M2/R12^3 .* D12;                         %Gravitational force between 1 and 2
-FG13 = - G*M1*M3/R13^3 .* D13;                         %Gravitational force between 1 and 3
-FG23 = - G*M2*M3/R23^3 .* D23;                         %Gravitational force between 2 and 3
+FG12 = - G*M1*M2/R12^3 .* D12;                    %Gravitational force between 1 and 2
+FG13 = - G*M1*M3/R13^3 .* D13;                    %Gravitational force between 1 and 3
+FG23 = - G*M2*M3/R23^3 .* D23;                    %Gravitational force between 2 and 3
 
-
+%R is cubed to find the unit vector
 FE12 = k * Q1*Q2 / R12^3 .*D12;             %Coulombic force between 1 and 2
 FE13 = k * Q1*Q3 / R13^3 .*D13;             %Coulombic force between 1 and 2
 FE23 = k * Q2*Q3 / R23^3 .*D23;             %Coulombic force between 2 and 3
@@ -24,8 +24,8 @@ P1_f = P1 + V1 *del_t;
 P2_f = P2 + V2 *del_t;
 P3_f = P3 + V3 *del_t;
 
-%R is cubed to find the unit vector
+
 V1_f = V1 + (FG12/M1 * del_t) + (FE12/abs(Q1) *del_t) +(FG13/M1 * del_t) + (FE13/abs(Q1) *del_t); %Equation of final velocity of M1
-V2_f = V2 - (FG12/M2 * del_t) - (FE12/abs(Q2) *del_t) -(FG23/M2 * del_t) + (FE23/abs(Q2) *del_t);   %Equation of final velocity of M1
-V3_f = V3 - (FG13/M3 * del_t) - (FE13/abs(Q3) *del_t) -(FG23/M3 * del_t) + (FE23/abs(Q3) *del_t);   %Equation of final velocity of M1
+V2_f = V2 - (FG12/M2 * del_t) - (FE12/abs(Q2) *del_t) +(FG23/M2 * del_t) + (FE23/abs(Q2) *del_t);   %Equation of final velocity of M1
+V3_f = V3 - (FG13/M3 * del_t) - (FE13/abs(Q3) *del_t) -(FG23/M3 * del_t) - (FE23/abs(Q3) *del_t);   %Equation of final velocity of M1
 
